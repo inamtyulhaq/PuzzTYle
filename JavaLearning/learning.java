472099509af6a0ba04d805725974badd345079c0
Inam Ul Haq <inamtyulhaq@gmail.com>
2024-08-07 12:19:40 +0500

    learning

--- JavaLearning/learning.java
+++ JavaLearning/learning.java
@@ -1,6 +1,13 @@
 public class learning {
 
     public static void main(String[] args) {
-        System.out.println("ada");
+        System.out.println("hello");
+        System.out.println("\t\"hello\"");
+        System.out.println("hello\n");
+        System.out.println("i am trying");
+
+        // spaces dont matter in java
+        /* multi line comment */
+
     }
 }
--- README.md
+++
@@ -1,3 +0,0 @@
-# PuzzTYle
-This application is built as a mid-term project for my "Software Engineering CC-212" course that I took in my 4th semester.
-My instructor for the course is <a href="https://www.linkedin.com/in/anzar-ahmad-93026b7/" target="_blank">Anzar Ahmad</a>.
